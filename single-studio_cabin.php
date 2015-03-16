e2719d8da7a93b23411b96564d17ccb5c95274b2
Rachel Weeks <rachelmweeks@gmail.com>
2015-03-15 19:06:12 -0500

    Updated Stylesheet for Painters Cabin

--- single-studio_cabin.php
+++ single-studio_cabin.php
@@ -4,32 +4,38 @@ Template Name: Studio Cabin
 */
 ?>
 
-<section class="cabinphoto">
-	<img><?php the_field('cabin_photo'); ?></img>
-</section>
 
-<section class="cabinpage">
+<?php get_header(); ?> 
 
-	</h1><?php echo get_the_title(); ?></h1>
+<main id="cabinpage">
 
-	<p><?php the_field('cabin_description'); ?></p>
+	<section class="cabinphoto">
+		<img src="<?php the_field('cabin_photo'); ?>" alt="photo of the cabin"/>
+	</section>
+
+	<section class="cabinpage">
+
+		</h1><?php echo get_the_title(); ?></h1>
+
+		<p><?php the_field('cabin_description'); ?></p>
 	
-	<h2>Art making features:</h2>
-	<p><?php the_field('features_art'); ?></p>
+		<h2>Art making features:</h2>
+		<p><?php the_field('features_art'); ?></p>
 	
-	<h2>Standard cabin features include:</h2>
-	<p><?php the_field('features_cabin'); ?></p>
+		<h2>Standard cabin features include:</h2>
+		<p><?php the_field('features_cabin'); ?></p>
 	
-	<h3>Once accepted <a>(application process)</a>:</h3>
-	<p><?php the_field('cost_mealplan'); ?> per night without <a>meal plan</a></p>
-	<p><?php the_field('cost_nomeal'); ?> per night with meal plan</p>
+		<h3>Once accepted <a>(application process)</a>:</h3>
+		<p>$<?php the_field('cost_mealplan'); ?> per night without <a>meal plan</a></p>
+		<p>$<?php the_field('cost_nomeal'); ?> per night with meal plan</p>
 	
-	<a class="button_green">check availability</a>
+		<a class="button_green">check availability</a>
 	
-</section>
+	</section>
 
+</main>
 
-
+<?php get_footer(); ?>
 
 
 
